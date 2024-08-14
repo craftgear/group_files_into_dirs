61a30a5af60d7b736f5be0d4e98490f420fd90d3
Shunsuke Watanabe <craftgear@gmail.com>
2024-08-14 11:16:51 +0900

    update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 cargo build --release
-tar -czvf release/macos.zip ./README.md -C target/release/ $(basename $(pwd))
+tar -czvf release/group_files_into_dirs\(macos\).zip ./README.md -C target/release/ $(basename $(pwd))
 
 cargo build --release --target x86_64-pc-windows-gnu
-tar -czvf release/windows.zip ./README.md -C target/x86_64-pc-windows-gnu/release/ $(basename $(pwd)).exe
+tar -czvf release/group_files_into_dirs\(windows\).zip ./README.md -C target/x86_64-pc-windows-gnu/release/ $(basename $(pwd)).exe
